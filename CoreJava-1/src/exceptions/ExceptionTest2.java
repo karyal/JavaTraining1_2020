c353bb501f480c42285805a51c81f89153b26999
= <=>
2020-11-20 05:15:05 +0545

    Checked and Unchecked Exceptions

--- CoreJava-1/src/exceptions/ExceptionTest2.java
+++ CoreJava-1/src/exceptions/ExceptionTest2.java
@@ -1,6 +1,7 @@
 package exceptions;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ExceptionTest2 {
@@ -11,13 +12,24 @@ public class ExceptionTest2 {
 	
 	//IOException - Checked Exception
 	public static String readString() {
-		InputStreamReader isr = new InputStreamReader(System.in);
-		BufferedReader br =  new BufferedReader (isr); 
-		//return "";
-		return br.readLine();
+		InputStreamReader isr ;
+		BufferedReader br;
+		String tmp_str="";
+		try {
+			isr = new InputStreamReader(System.in);
+			br =  new BufferedReader (isr); 
+			tmp_str = br.readLine(); //Checked Exception
+		}
+		catch(IOException ex) {//Checked Exception
+			System.out.println(ex.getMessage());
+		}
+		catch(Exception ex) {//Unchecked Exception
+			System.out.println(ex.getMessage());
+		}
+		return (tmp_str);
 	}
 	
-	public static String readString(String msg) {
+	public static String readString(String msg) throws IOException, Exception{
 		System.out.print(msg);
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br =  new BufferedReader (isr); 
