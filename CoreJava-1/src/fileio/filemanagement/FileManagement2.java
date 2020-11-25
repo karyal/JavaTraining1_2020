8aacda56f64c07571fda26abb933bd53e7cd2e54
= <=>
2020-11-25 06:11:36 +0545

    File Management

--- CoreJava-1/src/fileio/filemanagement/FileManagement2.java
+++ CoreJava-1/src/fileio/filemanagement/FileManagement2.java
@@ -4,12 +4,12 @@ import java.io.IOException;
 
 public class FileManagement2 {
 
-	public static void main(String[] args) throws IOException{
-		
-		File f1 = new File("data1.txt");
+	public static void main(String[] args) throws IOException {
+
+		File f1 = new File("data2.txt");
 		File f2 = new File("data2.txt");
 		File f3 = new File("data3.txt");
-		
+
 		boolean result = f1.exists();
 		System.out.println(result);		
 		if (result==false) {
@@ -18,12 +18,12 @@ public class FileManagement2 {
 		}
 		else {
 			System.out.println("File already exists");
-			
+
 			//Test
 			System.out.println("Execute : "+f1.canExecute());
 			System.out.println("Read : "+f1.canRead());
 			System.out.println("Write : "+f1.canWrite());
-			
+
 			int result1 = f1.compareTo(f2);
 			if (result1==0) {
 				System.out.println("Compare : Equals");
@@ -31,7 +31,7 @@ public class FileManagement2 {
 			else {
 				System.out.println("Compare : Not Equals");
 			}
-			
+
 			//Delete
 			if(f3.exists()==true) {
 				if(f3.delete()==true) {
@@ -44,7 +44,62 @@ public class FileManagement2 {
 			else {
 				System.out.println("File does not exists.");
 			}
+
+			//Path
+			String path1 = f1.getAbsolutePath();
+			System.out.println("Path : "+path1);
+
+			String path2 = f1.getCanonicalPath();
+			System.out.println("Path : "+path2);
+
+			//Name, Parent
+			String name = f1.getName();
+			String parent = f1.getParent();
+			String path3 = f1.getPath();
+
+			System.out.println("Name : "+name);
+			System.out.println("Parent: "+parent);
+			System.out.println("Path: "+path3);
+
+			//totalspace
+			long total_space = f1.getTotalSpace();
+			System.out.println("Total Space : "+total_space/1024/1024/1024+" GB");
+
+			if(f1.isDirectory()==true) {
+				System.out.println("f1 is Directory");
+			}
+			else {
+				System.out.println("f1 is File");
+			}
+
+			//last modified
+			Long last_modified = f1.lastModified();
+			System.out.println("Last Modified "+last_modified);//1606260703039 - timstamp (unix system) -> Date/time format
+
+			//length
+			long length = f1.length();
+			System.out.println("Length : "+length);
+
+			//hashcode
+			int hash_code1 = f1.hashCode();
+			System.out.println("Hashcode: "+hash_code1);
+			int hash_code2 = f2.hashCode();
+			System.out.println("Hashcode: "+hash_code2);
+
+			if(hash_code1==hash_code2) {
+				System.out.println("f1 is Equals with f2");
+			}
+			else {
+				System.out.println("f1 is not Equals with f2");
+			}
+			
+			//Equals
+			if(f1.equals(f2)) {
+				System.out.println("f1 is Equals with f2");
+			}
+			else {
+				System.out.println("f1 is not Equals with f2");
+			}
 		}
-		
 	}
 }
