5a94d8b2a5f5e3748a5f66d84765581753b19502
= <=>
2020-12-03 05:28:40 +0545

    Collections, Exceptions, Threads, jdbc

---
+++ CoreJava-1/src/jdbc/DatabaseTest.java
@@ -0,0 +1,11 @@
+package jdbc;
+
+public class DatabaseTest {
+
+	public static void main(String[] args) {
+		MyDatabase mdb = new MyDatabase();
+		//mdb.connectDb();
+		//mdb.insert();
+		mdb.select();
+	}
+}
---
+++ CoreJava-1/src/jdbc/MyDatabase.java
@@ -0,0 +1,112 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
+public class MyDatabase {
+	
+	public void connectDb() {
+		//Step-1
+		//Pre-requirements
+		String driver="com.mysql.cj.jdbc.Driver";
+		String host="127.0.0.1";
+		int port = 3306;
+		String db_name="java1";
+		String db_user="admin";
+		String user_pass="admin@123";
+		
+		try {
+			//Step-2
+			//Load Driver
+			System.out.println("Connecting.......");
+			Class.forName(driver);
+			//Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/java1?", "admin", "admin@123");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://"+ host +":"+ port +"/"+ db_name +"?", db_user, user_pass);
+			System.out.println("Connected......");
+			conn.close();
+			System.out.println("Close Connection");
+		}
+		catch(Exception ex) {
+			System.out.println("Error : "+ex.getMessage());
+		}
+	}
+	
+	public void insert() {
+		//Step-1
+		//Pre-requirements
+		String driver="com.mysql.cj.jdbc.Driver";
+		String host="127.0.0.1";
+		int port = 3306;
+		String db_name="java1";
+		String db_user="admin";
+		String user_pass="admin@123";
+		
+		String str_sql = "INSERT INTO contacts VALUES(107, 'kRISHNA','NEP','krishna@gmail.com','9843212345')";
+		
+		try {
+			//Step-2
+			//Load Driver
+			System.out.println("Connecting.......");
+			Class.forName(driver);
+			//Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/java1?", "admin", "admin@123");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://"+ host +":"+ port +"/"+ db_name +"?", db_user, user_pass);
+			System.out.println("Connected......");
+			
+			//Insert Record
+			System.out.println("Inserting.....");
+			Statement stat = conn.createStatement();
+			stat.executeUpdate(str_sql); //executeUpdate-> Insert, Update, Delete Record
+			stat.close();
+			System.out.println("Inserted.....");
+			conn.close();
+			System.out.println("Close Connection");
+		}
+		catch(Exception ex) {
+			System.out.println("Error : "+ex.getMessage());
+		}
+	}
+	
+	public void select() {
+		//Step-1
+		//Pre-requirements
+		String driver="com.mysql.cj.jdbc.Driver";
+		String host="127.0.0.1";
+		int port = 3306;
+		String db_name="java1";
+		String db_user="admin";
+		String user_pass="admin@123";
+		
+		String str_sql = "SELECT * FROM contacts ORDER BY sn ASC LIMIT 1000";
+		
+		try {
+			//Step-2
+			//Load Driver
+			//System.out.println("Connecting.......");
+			Class.forName(driver);
+			//Connection conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/java1?", "admin", "admin@123");
+			Connection conn = DriverManager.getConnection("jdbc:mysql://"+ host +":"+ port +"/"+ db_name +"?", db_user, user_pass);
+			//System.out.println("Connected......");
+			
+			//Select Record
+			//System.out.println("Selecting.....");
+			Statement stat = conn.createStatement();						
+			
+			ResultSet rs = stat.executeQuery(str_sql); //executeQuery-> Select 
+			
+			System.out.println("SN\tNAME\tADDRESS\tEMAIL\t\t\tPHONE");
+			while(rs.next()) {
+				System.out.println(rs.getInt("sn")+"\t"+rs.getString("name")+"\t"+rs.getString("address")+"\t"+rs.getString("email")+"\t\t"+rs.getString("phone"));
+			}
+			
+			stat.close();
+			//System.out.println("Selected.....");
+			conn.close();
+			//System.out.println("Close Connection");
+		}
+		catch(Exception ex) {
+			System.out.println("Error : "+ex.getMessage());
+		}
+	}
+}
